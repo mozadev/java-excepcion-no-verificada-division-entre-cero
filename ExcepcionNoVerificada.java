bab03e9768d8b3375c39a2bf11327b5078d9e0fd
Cesar <30559667+CesarAroon@users.noreply.github.com>
2020-12-16 20:59:51 -0500

    Add files via upload

---
+++ ExcepcionNoVerificada.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepcionnoverificada;
+
+
+public class ExcepcionNoVerificada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=5, n2=0, resultado;
+        
+        resultado=n/n2 ;
+        System.out.println(resultado);
+    }
+    
+}
